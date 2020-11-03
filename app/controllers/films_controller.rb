227865f693c60630678f1f6007ca99311f62ef82
Hiroki Shiroyama <hs@HirokinoMacBook-Pro.local>
2020-11-03 23:46:02 +0900

    filmのindexとnewビューを作成

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,2 +1,37 @@
 class FilmsController < ApplicationController
+  def index
+    @films = Film.all
+  end
+
+  def show
+  end
+
+  def new
+    @film = Film.new
+  end
+
+  def create
+    @film = Film.new(film_params)
+    if @film.save
+      flash[:success] = "新しいフィルムを登録しました！"
+      redirect root_url
+    else
+      render 'films/new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def film_params
+      params.require(:film).permit(:name, :company, :iso)
+    end
+
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,4 @@
 class Film < ApplicationRecord
-  validates :name, :presence true, length: { maximum: 127 }
-  validates :iso, :presence true
+  validates :name, presence: true, length: { maximum: 127 }
+  validates :iso, presence: true
 end
