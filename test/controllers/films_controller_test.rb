c375de8d0b760ba5c078c566f86c56cbec987ae2
Hiroki Shiroyama <hs@HirokinoMacBook-Pro.local>
2020-11-08 19:39:25 +0900

    filmモデルとコントローラに対してテストを記述

--- test/controllers/films_controller_test.rb
+++ test/controllers/films_controller_test.rb
@@ -4,4 +4,24 @@ class FilmsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "indexアクションをgetできるか" do
+    get films_url
+    assert_response :success
+  end
+
+  test "newアクションをgetできるか" do
+    get new_film_url
+    assert_response :success
+  end
+
+  # test "editアクションをgetできるか" do
+  #   get edit_film_url
+  #   assert_response :success
+  # end
+
+  # test "showアクションをgetできるか" do
+  #   get film_url
+  #   assert_response :success
+  # end
 end
--- test/models/film_test.rb
+++ test/models/film_test.rb
@@ -4,4 +4,27 @@ class FilmTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @film = Film.new(name: "example_name", iso: "100")
+  end
+
+  test "有効なFilmかどうかテスト" do
+    assert @film.valid?
+  end
+
+  test "空白の名前に対するバリデーションが機能するかテスト" do
+    @film.name = "     "
+    assert_not @film.valid?
+  end
+
+  test "空白のisomに対するバリデーションが機能するかテスト" do
+    @film.iso = "      "
+    assert_not @film.valid?
+  end
+
+  test "長すぎる名前に対するバリデーションが機能するかテスト" do
+    @film.name = "a" * 128
+    assert_not @film.valid?
+  end
 end
