911dccd1b79417bd0af5b38f47dc4bef90d2a5f4
Hiroki Shiroyama <hs@HirokinoMacBook-Pro.local>
2020-11-11 22:44:02 +0900

    ログインのためのsessionsコントローラ、ビューを作成

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "誤った情報を渡したログイン処理" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: {
+      session: {
+        email: "",
+        password: ""
+      }
+    }
+    assert_template 'sessions/new'
+    # assert_not flash.empty?
+    # get root_url
+    # assert flash.empty?
+  end
+end
