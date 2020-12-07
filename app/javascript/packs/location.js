cc81b83b17553d4b53dd0b11e161991267dc64cd
Hiroki Shiroyama <hs@HirokinoMacBook-Pro.local>
2020-12-08 01:32:15 +0900

    google mapを表示

---
+++ app/javascript/packs/location.js
@@ -0,0 +1,48 @@
+function successFunc(position) {
+  var data = position.coords; //取得した位置情報を整理
+  var lat = data.latitude; //緯度
+  var lng = data.longitude; //経度
+
+  alert("緯度：" + lat + "　経度：" + lng);
+  var phtotoLatLng = new google.maps.LatLng(lat, lng);
+  var option = {
+    zoom: 15,
+    center: phtotoLatLng,
+  };
+  var map = new google.maps.Map(document.getElementById("map"), option);
+  new google.maps.Marker({
+    map: map,
+    position: phtotoLatLng
+  });
+}
+
+function errorFunc(error){
+  var errorMessage = {
+    0: "原因不明のエラーが発生しました",
+    1: "位置情報の取得が許可されませんでした",
+    2: "電波状況により位置情報が取得できませんでした",
+    3: "タイムアウトしました",
+  };
+
+  alert(errorMessage[error.code]);
+}
+
+var optionObj = {
+  "enableHighAccuracy": false ,
+	"timeout": 8000 ,
+	"maximumAge": 5000 ,
+};
+
+document.getElementById("text-button").onclick = function(){ //htmlのid"text_button"が押された時
+// 使用端末が位置情報に対応している場合
+  if (navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(successFunc, errorFunc, optionObj);
+  }
+  // 対応していない場合
+  else
+  {
+    alert("お使いの端末はGeoLocation APIに対応していません");
+  }
+};
+
