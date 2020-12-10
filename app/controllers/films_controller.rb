348409282d8be54472b256a488d8c156afc0c2dd
Hiroki Shiroyama <hs@HirokinoMacBook-Pro.local>
2020-12-11 00:59:15 +0900

    film indexを消去

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,9 +1,6 @@
 class FilmsController < ApplicationController
   before_action :logged_in_user
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  def index
-    @films = Film.all
-  end
 
   def show
     @photos = @film.photos.paginate(page: params[:page])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :films
+  resources :films, [:show,:new, :create, :edit, :update, :destroy]
   resources :photos, only: [:show, :create, :edit, :update, :destroy]
   resource :user #単数系リソースの使用
 end
