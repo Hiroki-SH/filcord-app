888dea84cb5af521f242246695509df677f098cb
Hiroki Shiroyama <hs@HirokinoMacBook-Pro.local>
2020-12-12 18:01:41 +0900

    タイトルをページごとに変更させるメソッドを追加

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+  #ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Filcord"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+end
