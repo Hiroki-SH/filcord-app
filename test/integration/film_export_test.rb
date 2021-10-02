34d39a5e019f7cbe31fd3c741d5115295708c203
Hiroki-SH <srymc75@gmail.com>
2021-10-02 13:15:08 +0900

    ファイル出力で一時ファイルの削除をする際、ファイル存在判定を追加

--- test/integration/film_export_test.rb
+++ test/integration/film_export_test.rb
@@ -4,24 +4,25 @@ class FilmExportTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:user1)
     @film = films(:film1)
+    @film2 = films(:film2)
   end
 
-  def request_film_export
+  def request_film_export(film)
     post film_export_path, params: {
-      id: @film.id
+      id: film.id
     }
   end
 
   test "撮影データをCSVファイルでダウンロードできるか" do
     log_in_as(@user)
-    request_film_export
+    request_film_export(@film)
     assert_match "text/csv", response.headers["Content-Type"]
     assert_match /attachment/, response.headers["Content-Disposition"]
   end
 
   test "撮影データのCSVファイルの内容が正しいか" do
     log_in_as(@user)
-    request_film_export
+    request_film_export(@film2)
     response.body.each_line.each_with_index do |line, idx|
       if idx == 0 
         assert_match "id,シャッタースピード,F値,撮影日", line
